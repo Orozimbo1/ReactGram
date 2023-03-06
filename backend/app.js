7a41046e019fad00bcd644499818f0cbb0464bd5
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-05 22:23:07 -0300

    Importando middlewares

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,15 @@ app.use(express.urlencoded({ extended: false }))
 // routes
 const router = require('./routes/Router')
 
+// Solve CORS
+app.use(cors({ credentials: true, origin: '*' }))
+
+// Upload directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// DB connection
+require('./config/db.js')
+
 app.use(router)
 
 app.listen(port, () => {
