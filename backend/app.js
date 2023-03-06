5ae5c14c7d074ebee218e176ca9b11483d1cdcef
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-05 22:16:59 -0300

    Rota de teste da API

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// routes
+const router = require('./routes/Router')
+
+app.use(router)
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
   console.log(`http://localhost:+ ${port}`)
