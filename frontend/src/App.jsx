1454fe20ec3c6f7cad7bd94151876736903bd6a5
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-09 21:56:15 -0300

    Configurando o React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
 
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// Pages
+import { Home, Login, Register } from './pages'
+
+// Components
+
+// Context
+
+// Hooks
+
+// Middlewares
+
 function App() {
 
   return (
-    <h1>ReactGram</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
