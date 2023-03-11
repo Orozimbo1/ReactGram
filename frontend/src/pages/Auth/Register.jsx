4dd6b4fed8812eeb448e12200fbd45be1b03e8d6
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-11 18:43:55 -0300

    Criando formulário de cadastro

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,9 +1,29 @@
 import './Auth.css'
 
+// Componentes
+import { Link } from 'react-router-dom'
+
+// Hooks
+import { useState, useEffect } from 'react'
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      <h1>Register</h1>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Confirme a sua senha' />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>Ja tem conta? <Link to='/login'>Clique aqui</Link></p>
     </div>
   )
 }
