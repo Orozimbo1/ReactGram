61565da80679ed8727273c7a5d92bd3c8226ad78
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-11 22:37:25 -0300

    Conectando formulário ao reducer

--- backend/middlewares/userValidation.js
+++ backend/middlewares/userValidation.js
@@ -17,7 +17,7 @@ const userCreateValidation = () => {
       .withMessage('A senha é obrigatória.')
       .isLength({ min: 6 })
       .withMessage('A senha precisa conter no mínimo 6 caracteres.'),
-    body('confirmpassword')
+    body('confirmPassword')
       .isString()
       .withMessage('A conffirmação de senha é obrigatória.')
       .custom((value, {req}) => {
