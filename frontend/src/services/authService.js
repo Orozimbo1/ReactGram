6e3befb70827945f0178676b8fdc157fb583af09
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-12 00:15:39 -0300

    Service de login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -17,7 +17,7 @@ const register = async (data) => {
 
     return res;
   } catch (error) {
-    console.log(error.message)
+    console.log(error)
   }
 
 }
@@ -27,9 +27,33 @@ const logout = () => {
   localStorage.removeItem('user')
 }
 
+// Sign in an user
+const login = async (data) => {
+
+  const config = requestConfig('POST', data);
+
+  try {
+
+    const res = await fetch(`${api}/users/login`, config)
+      .then((res) => res.json())
+      .catch((err) => console.log(err))
+    
+    if(res) {
+      localStorage.setItem('user', JSON.stringify(res))
+    }
+
+    return res;
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
 const authService = {
   register,
   logout,
+  login,
 }
 
 export default authService
