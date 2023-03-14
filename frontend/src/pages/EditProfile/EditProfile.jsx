c1272dcd9fc77c63bfc0f91afa05312bcf7c062c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-14 20:22:49 -0300

    Exibindo a imagem de preview

--- frontend/src/pages/EditProfile/EditProfile.jsx
+++ frontend/src/pages/EditProfile/EditProfile.jsx
@@ -44,11 +44,31 @@ const EditProfile = () => {
     e.preventDefault()
   }
 
+  const handleFile = (e) => {
+    // Image preview
+    const image = e.target.files[0]
+
+    setPreviewImage(image)
+
+    // Update image state
+    setProfileImage(image)
+  }
+
   return (
     <div id='edit-profile'>
       <h2>Edite seus dados</h2>
       <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
-      {/* Preview da imagem */}
+      {(user.profileImage || previewImage) && (
+        <img 
+          className='profile-image'
+          src={
+            previewImage 
+            ? URL.createObjectURL(previewImage) 
+            : `${uploads}/users/${user.profileImage}`
+          }
+          alt={user.name}
+        />
+      )}
       <form onSubmit={handleSubmit}>
         <input 
           type="text" 
@@ -64,7 +84,7 @@ const EditProfile = () => {
         />
         <label>
           <span>Imagem do Perfil:</span>
-          <input type="file" />
+          <input type="file" onChange={handleFile} />
         </label>
         <label>
           <span>Bio:</span>
