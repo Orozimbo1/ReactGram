8f4f0e32e8058aa64cca115758e679b4e52ee1fb
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-13 22:11:41 -0300

    Slice perfil do usuário

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import userService from '../services/userService'
 
 const initialState = {
   user: {},
@@ -8,6 +9,20 @@ const initialState = {
   message: null
 }
 
+// Get user details
+export const profile = createAsyncThunk(
+  'users/profile',
+  async (user, thunkAPI) => {
+
+    const token = thunkAPI.getState().auth.user.token
+
+    const data = await userService.profile(user, token)
+
+    return data;
+
+  }
+) 
+
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -15,6 +30,18 @@ export const userSlice = createSlice({
     resetMessages: (state) => {
       state.message = null
     },
+  },
+  extraReducers: (builders) => {
+    builders.addCase(profile.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    })
+    .addCase(profile.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = false;
+      state.user = action.payload;
+    })
   }
 })
 
