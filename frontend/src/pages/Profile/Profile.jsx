7a3df8e204fbb83e1700b62e86ed67fa59e1079c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-14 21:30:28 -0300

    Service para resgatar dados do usuário por id

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -12,6 +12,8 @@ import { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
+// Redux
+
 const Profile = () => {
   return (
     <div>
