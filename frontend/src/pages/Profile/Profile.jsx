ce4f694d5fb85cb3538c5e746f3da351fc20f2ac
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-14 21:43:06 -0300

    Exibindo dados no perfil

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import { uploads } from '../../utils/config'
 // Components
 import Message from '../../components/Messages/Message'
 import { Link } from 'react-router-dom'
-import { BsFillEyeFill, BsPencilFill, BsXlg } from 'react-icons/bs'
+import { BsFillEyeFill, BsPencilFill, BsXLg } from 'react-icons/bs'
 
 // Hooks
 import { useState, useEffect, useRef } from 'react'
@@ -13,11 +13,38 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 // Redux
+import { getUserDetails } from '../../slices/userSlice'
 
 const Profile = () => {
+  const { id } = useParams()
+
+  const dispatch = useDispatch()
+
+  const { user, loading } = useSelector((state) => state.user)
+  const { user: userAuth } = useSelector((state) => state.auth)
+
+  // Photo
+
+  // Load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id))
+  }, [dispatch, id])
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div id='profile'>
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p className='subtitle'>{user.bio}</p>
+        </div>
+      </div>
     </div>
   )
 }
