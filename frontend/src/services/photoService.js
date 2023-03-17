fd2568ac7a73012986e23f7c7102535f31249305
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-17 19:44:14 -0300

    Service de todas as fotos

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -125,6 +125,24 @@ const comment = async (data, id, token) => {
 
 }
 
+// Get all photos 
+const getPhotos = async (token) => {
+
+  const config = requestConfig('GET', null, token)
+
+  try {
+    
+    const res = await fetch(`${api}/photos`, config)
+      .then((res) => res.json())
+      .catch((err) => err)
+
+    return res
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -133,6 +151,7 @@ const photoService = {
   getPhotoById,
   like,
   comment,
+  getPhotos,
 }
 
 export default photoService;
