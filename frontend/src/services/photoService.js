7cc07e3a241e4c70aa1152e73017cec4cf230e42
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-17 20:38:32 -0300

    Service da busca

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -143,6 +143,23 @@ const getPhotos = async (token) => {
   }
 }
 
+// Search photo by title
+const searchPhotos = async (query, token) => {
+
+  const config = requestConfig('GET', null, token)
+
+  try {
+    
+    const res = await fetch(`${api}/photos/search?q=${query}`, config)
+      .then((res) => res.json())
+      .catch((err) => err)
+
+    return res;  
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
